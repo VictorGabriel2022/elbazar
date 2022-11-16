bebfc2c975db2a0771dcc193373b6660882dc37a
VictorGabriel2022 <mazmorra13rol@gmail.com>
2022-11-15 23:46:06 -0500

    Elbazar

---
+++ elbazar/src/principal/ComponentePrincipal.tsx
@@ -0,0 +1,8 @@
+export default function ComponentePrincipal(){
+    return(
+       <div>
+        <h1>Principal</h1>
+        <p>Es la información de la pagina principal</p>
+       </div>
+    );
+}
